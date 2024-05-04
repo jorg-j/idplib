6b34d64df9508b05ffafb6e8542e5572c8096b60
jorg-j <jorgensen.server@gmail.com>
2024-05-04 11:44:45 +1000

    add new tests for credit cards and abn checks

---
+++ tests/test_identify.py
@@ -0,0 +1,36 @@
+import os.path as path
+import sys
+import unittest
+from unittest.mock import ANY
+from idplib import ValueUtils
+
+class test_identify_values(unittest.TestCase):
+    def test_identify_creditcard(self):
+
+        result = ValueUtils.Identify.credit_card_number('7949519628')
+
+        self.assertEqual(result, True)
+
+    def test_identify_invalid_creditcard(self):
+
+        result = ValueUtils.Identify.credit_card_number('111111111111')
+
+        self.assertEqual(result, False)
+
+
+    def test_identify_abn(self):
+
+        result = ValueUtils.Identify.abn('51 824 753 556')
+
+        self.assertEqual(result, True)
+
+    def test_identify_invalid_abn(self):
+
+        result = ValueUtils.Identify.abn('111111111111')
+
+        self.assertEqual(result, False)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
